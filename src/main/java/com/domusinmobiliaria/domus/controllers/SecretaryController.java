77f5edad360d1a5fa9aaad1c3ad9d3b8ae852155
ezequieljm <ezequieljm020@gmail.com>
2021-11-12 04:16:43 +0000

    Request Get implemented

--- src/main/java/com/domusinmobiliaria/domus/controllers/SecretaryController.java
+++ src/main/java/com/domusinmobiliaria/domus/controllers/SecretaryController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-@RequestMapping(value = SecretaryAppointmentsController.BASIS)
-public class SecretaryAppointmentsController
+@RestController
+@RequestMapping(value = SecretaryController.BASIS)
+public class SecretaryController
 {
     public static final String BASIS = "/secretary";
     public static final String ID_ID = "/{id}";
@@ -35,13 +35,13 @@ public class SecretaryAppointmentsController
         return "Shedule";
     }
 
-    @GetMapping(value = "/schedule/modify-appointments")
+    @GetMapping(value = "/schedule/appointments")
     public ResponseEntity<List<Appointment>> appointmentsResourse()
     {
         return ResponseEntity.ok(appointmentService.findAll());
     }
 
-    @GetMapping(value = "/schedule/modify-appointments/" + ID_ID)
+    @GetMapping(value = "/schedule/appointments/" + ID_ID)
     public ResponseEntity<Optional<Appointment>> anAppointment(@PathVariable(value = "id") Long id)
     {
         return ResponseEntity.ok(appointmentService.findById(id));
